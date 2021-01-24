3edfdca4ec7ee8de8ecb85001ace151cb304b52f
Riskal Abdullah Balfas <riskalab@gmail.com>
2021-01-24 19:52:55 +0700

    Updates

---
+++ precache-manifest.0d914f6562bbc1afdad7a4e362c104b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fe20daabccecd76a47ed2990046256",
+    "url": "/sport-unite/index.html"
+  },
+  {
+    "revision": "4dd7d7ea9f3ce21b712b",
+    "url": "/sport-unite/static/css/main.a8c37309.chunk.css"
+  },
+  {
+    "revision": "a130e32670fe5b495d36",
+    "url": "/sport-unite/static/js/2.51f7bde2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sport-unite/static/js/2.51f7bde2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dd7d7ea9f3ce21b712b",
+    "url": "/sport-unite/static/js/main.34888c10.chunk.js"
+  },
+  {
+    "revision": "714448ffc209c8369c26",
+    "url": "/sport-unite/static/js/runtime-main.31d3d5f2.js"
+  },
+  {
+    "revision": "2e34f5e62725b71bf26e05fe2fb3c175",
+    "url": "/sport-unite/static/media/draw-color.2e34f5e6.png"
+  },
+  {
+    "revision": "1257796f3a0dca41a7c28bf16be425d6",
+    "url": "/sport-unite/static/media/kalUnite(Light).1257796f.png"
+  },
+  {
+    "revision": "668576a5667d378cd894d7c03e31f9f5",
+    "url": "/sport-unite/static/media/lose-color.668576a5.png"
+  },
+  {
+    "revision": "05b7b23d362f1c885688479c0d9a1af5",
+    "url": "/sport-unite/static/media/win-color.05b7b23d.png"
+  }
+]);
